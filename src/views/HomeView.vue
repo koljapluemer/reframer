3f2c24d7004d11fa77bbb6ecbb30abe5d6534b59
blackbrokkoli <kastomat@gmail.com>
2022-11-19 17:47:10 +0100

    build very simple POC on generated Vue app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="showProblemInput">
+    <h2>Jot down a problem:</h2>
+    <input name="" id="" cols="30" rows="10" v-model="problem">
+  </div>
+  <div v-else>
+    <h3>
+      Reframe {{ problem }} into {{ frame }}:
+    </h3>
+  </div>
+
+  <button @click="reframe()">Reframe!</button>
+  <button @click="showProblemInput = !showProblemInput">Toggle Problem Input</button>
+</template>
+
+<script >
+export default {
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      problem: "",
+      showProblemInput: true,
+      frames: [
+        'Algorithm',
+        'SMART Goal',
+        'Business Model Canvas',
+        'Ockham\'s Razor',
+      ],
+      frame: null
+    };
+  },
+  methods: {
+    reframe() {
+      this.showProblemInput = false;
+      this.frame = this.frames[Math.floor(Math.random() * this.frames.length)];
+    },
+  },
+  computed: {},
+  mounted() {},
+  beforeDestroy() {},
+};
+</script>
